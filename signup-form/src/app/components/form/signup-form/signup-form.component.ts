f96ab0105da5cb0da3ab84816f94f613c2bac2af
bervalim <bernardogvalim@gmail.com>
2024-04-05 16:24:48 -0300

    feat/formGroup-implemented

--- signup-form/src/app/components/form/signup-form/signup-form.component.ts
+++ signup-form/src/app/components/form/signup-form/signup-form.component.ts
@@ -1,12 +1,25 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-signup-form',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './signup-form.component.html',
-  styleUrl: './signup-form.component.scss'
+  styleUrl: './signup-form.component.scss',
 })
 export class SignupFormComponent {
+  signupForm = new FormGroup({
+    name: new FormControl(null),
+    email: new FormControl(null),
+    password: new FormControl(null),
+    phone: new FormControl(null),
+    sex: new FormControl(''),
+  });
 
+  submitSignUpForm() {
+    console.log(this.signupForm.value);
+    this.signupForm.reset({ sex: '' });
+  }
 }
